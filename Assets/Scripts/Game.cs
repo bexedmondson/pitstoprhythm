14dbce42c9941bf9863bb74e24ecf95a69f0672c
Bex Edmondson <bex.edmondson@hutchgames.com>
2019-01-03 11:33:18 +0000

    adding in basic scripts

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour 
+{
+	public float m_audioLength = 10f;
+
+	private float m_timeSinceStart = 0f;
+
+
+	void Start () 
+	{
+		
+	}
+
+	void FixedUpdate () 
+	{      
+		if (m_timeSinceStart < m_audioLength)
+		{
+			//do stuff
+			m_timeSinceStart += Time.deltaTime;
+			Debug.Log(m_timeSinceStart.ToString());
+		}
+		else
+		{
+			EndGame();
+		}
+	}
+
+	private void EndGame()
+	{
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+         Application.Quit();
+#endif
+	}
+}
